0f268522674b43cff9a31a7f9f4644d9f169f664
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 23:08:23 +0700

    Python if .. elif .. else statement

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 if __name__ == "__main__":
   # The simple Python if statement
 
-  # age = input("Enter your age: ")
-  # if int(age) >= 18:
-  #   print("You're eligible to vote.")
-  #   print("Let's go and vote.")
+  age = int(input("Enter your age: "))
+  if age >= 18:
+    print("You're eligible to vote.")
+    print("Let's go and vote.")
 
   # Python if ... else statement
 
-  age = input("Enter your age: ")
-  if int(age) >= 18:
+  if age >= 18:
     print("You're eligible to vote.")
   else:
     print("You're not eligible to vote.")
+
+  # Python if .. elif .. else statement
+
+  if age < 5:
+    ticket_price = 5
+  elif age < 16:
+    ticket_price = 10
+  else:
+    ticket_price = 18
+
+  print(f"You'll pay {ticket_price} for the ticket")
