a769652318005238cc5f7a98d62fc04efeafab66
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-24 22:59:53 +0700

    The simple Python if statement

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Python If Statement
 
 - Reference: https://www.pythontutorial.net/python-basics/python-if
+
+- Run script
+```python
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+  # The simple Python if statement
+
+  age = input("Enter your age: ")
+  if int(age) >= 18:
+    print("You're eligible to vote.")
+    print("Let's go and vote.")
